46575946e5f5f056dd3f17e3d7ca985b3fecec3d
Doug Keller <dkeller@patriotsoftware.com>
2018-10-10 22:16:39 -0400

    Add a player service

---
+++ webclient/src/app/modules/common/services/player.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Player } from '@dm/common/models/player';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerService {
+  private playersUrl = './assets/players.json';
+  private request;
+  private players;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private mapResponse(results: any): Player[] {
+    return results.map((r) => new Player(r))
+  }
+
+  getPlayers(): Observable<Player[]> {
+    return this.http.get<Object>(this.playersUrl).pipe(map(this.mapResponse));
+  }
+}
